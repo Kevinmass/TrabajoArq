da7b163324e55c9220f4f1041a749a57ae1c552f
Kevinmass <michilover@duck.com>
2024-06-10 12:58:19 -0300

    menu completo

--- C/main.c
+++ C/main.c
@@ -9,7 +9,16 @@ void delay (int);
 void main (void)
 {
 int option;
-
+int ctr;
+for (int i = 0; i < 3; i++)
+{
+   ctr = password();
+}
+   if (ctr != 1)
+   {
+      return 0;
+   }
+   
    printf("Menu:\n");
    printf("1. Auto Fantastico\n");
    printf("2. Choque\n");
@@ -20,10 +29,10 @@ int option;
 
    switch (option) {
       case 1:
-         // Code for Option 1
+         auto_fantastico();
          break;
       case 2:
-         // Code for Option 2
+         choque();
          break;
       case 3:
          // Code for Option 3
@@ -37,5 +46,35 @@ int option;
       default:
          printf("Opcion invalida.\n");
          break;
+   }
+}
+
+int password(){
+   int ctr = 0;
+   char pass[4];
+   char pass2[4] = {'1', '2', '3', '4'};
+   printf("Ingrese la contrasena: ");
+   for (int i = 0; i < 4; i++)
+   {
+      pass[i] = getch();
+      printf("*");
+   }
+   printf("\n");
+   for (int i = 0; i < 4; i++)
+   {
+      if (pass[i] == pass2[i])
+      {
+         ctr++;
+      }
+   }
+   if (ctr == 4)
+   {
+      printf("Contrasena correcta.\n");
+      return 1;
+   }
+   else
+   {
+      printf("Contrasena incorrecta.\n");
+      return 0;
    }
 }
